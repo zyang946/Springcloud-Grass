b749b7938012e94d9ef8b243f3158330fbd1d338
zyang946 <zhangyang946@163.com>
2022-12-05 16:17:40 +0800

    update gateway && update xxxmain -> xxxApplication

--- provide-api/test-api/src/main/java/com/springboot/cloud/TestApiApplication.java
+++ provide-api/test-api/src/main/java/com/springboot/cloud/TestApiApplication.java
@@ -1,5 +1,6 @@
 package com.springboot.cloud;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -8,8 +9,8 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableSwagger2
-public class TestMain {
+public class TestApiApplication {
     public static void main(String[] args) {
-        SpringApplication.run(TestMain.class);
+        SpringApplication.run(TestApiApplication.class);
     }
 }
--- provide/test/src/main/java/com/springboot/cloud/TestApplication.java
+++ provide/test/src/main/java/com/springboot/cloud/TestApplication.java
@@ -1,6 +1,5 @@
 package com.springboot.cloud;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -9,8 +8,8 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableSwagger2
-public class TestApiMain {
+public class TestApplication {
     public static void main(String[] args) {
-        SpringApplication.run(TestApiMain.class);
+        SpringApplication.run(TestApplication.class);
     }
 }
