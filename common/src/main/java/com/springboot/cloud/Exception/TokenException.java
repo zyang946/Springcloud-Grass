9b584b575fd242758b970692c1be028e95de56c2
zyang946 <zhangyang946@163.com>
2022-12-05 19:37:28 +0800

    gitgonre

--- common/src/main/java/com/springboot/cloud/Exception/TokenException.java
+++
@@ -1,8 +0,0 @@
-package com.springboot.cloud.Exception;
-
-public class TokenException extends Exception{
-
-    public TokenException(String message) {
-        super(message);
-    }
-}
--- provide/test/src/main/java/com/springboot/cloud/Service/TestService.java
+++
@@ -1,9 +0,0 @@
-package com.springboot.cloud.Service;
-
-
-import com.springboot.cloud.util.Response;
-import org.springframework.http.HttpHeaders;
-
-public interface TestService {
-    public Response test(Integer id, HttpHeaders headers);
-}
