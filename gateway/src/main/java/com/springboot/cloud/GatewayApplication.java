c4bd373b3dbe03e3c1529e74b175646bb3e7d3d1
zyang946 <zhangyang946@163.com>
2022-12-05 20:01:14 +0800

    update project strtucture

--- gateway/src/main/java/com/springboot/cloud/GatewayApplication.java
+++ gateway/src/main/java/com/springboot/cloud/GatewayApplication.java
@@ -1,7 +1,6 @@
 package com.springboot.cloud;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
