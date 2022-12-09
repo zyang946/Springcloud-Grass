bf84841fff308df12229ed3ba361ef4d9890e8c8
zyang946 <zhangyang946@163.com>
2022-12-09 21:06:37 +0800

    delete error

--- provide-api/code-generate-api/src/main/java/com/springboot/cloud/Service/CodeGenerateService.java
+++
@@ -1,9 +0,0 @@
-package com.springboot.cloud.service;
-
-import com.springboot.cloud.dto.TableDto;
-import com.springboot.cloud.utils.Response;
-import org.springframework.http.HttpHeaders;
-
-public interface CodeGenerateService {
-    public Response genereateCode(TableDto tableDto, HttpHeaders header);
-}
